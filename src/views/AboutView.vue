967ab4c4b243ff28fdfcfeab1968c0b03f1ff916
Safayet <hossainsafayet187@gmail.com>
2025-08-05 00:37:01 +0600

    text size fixed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1 class="text-xs text-red-700">This is an about page</h1>
+    <h1 class="text-4xl text-red-700">This is an about page</h1>
   </div>
 </template>
 
