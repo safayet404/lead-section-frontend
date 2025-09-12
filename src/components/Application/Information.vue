8ca3427a42ea632331807e49ad8c581c0be4772c
Safayet <hossainsafayet187@gmail.com>
2025-09-13 01:42:37 +0600

    information section complete

--- src/components/Application/Information.vue
+++ src/components/Application/Information.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from 'vue'
+
 const props = defineProps({
   country: {
     type: [String, Number],
@@ -21,11 +23,18 @@ const props = defineProps({
     required: true,
   },
 })
+
+let activeTab = ref('academic')
+
+let tab = [
+  { name: 'academic', label: 'Academic Requirement' },
+  { name: 'english', label: 'English Requirement' },
+]
 </script>
 
 <template>
-  <div class="container p-4">
-    <div class="grid grid-cols-4">
+  <div class="container p-4 mx-auto">
+    <div class="grid grid-cols-4 gap-5 items-start">
       <div class="col-span-1 p-3 border border-gray-200 shadow-2xl rounded-xl">
         <ul class="space-y-6">
           <li>
@@ -42,10 +51,34 @@ const props = defineProps({
           <li><span class="font-semibold"> Country : </span> {{ props.country.name }}</li>
         </ul>
       </div>
-      <div class="col-span-3"></div>
+      <div class="col-span-3 p-4">
+        <div class="border border-gray-200 rounded-xl shadow-2xl">
+          <button
+            v-for="t in tab"
+            :key="t.name"
+            @click="activeTab = t.name"
+            :class="[
+              'm-3 cursor-pointer ',
+              activeTab === t.name ? 'border-b-2 border-purple-700 font-medium' : '',
+            ]"
+          >
+            {{ t.label }}
+          </button>
+          <div class="m-3" v-if="activeTab == 'academic'">
+            {{ props.course.academic_requirement }}
+          </div>
+          <div class="m-3" v-else>
+            {{ props.course.english_requirement }}
+          </div>
+        </div>
+
+        <div class="flex justify-between mt-5 m-1">
+          <button class="px-7 py-2 rounded text-white bg-gray-500">Back</button>
+          <button class="px-7 py-2 rounded bg-purple-600 text-white font-medium">Next</button>
+        </div>
+      </div>
     </div>
   </div>
-  <h1>This is Information - {{ props.country }}</h1>
 </template>
 
 <style scoped></style>
